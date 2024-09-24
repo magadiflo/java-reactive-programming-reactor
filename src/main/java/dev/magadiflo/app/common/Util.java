c4be22575c912fbc1b49a6ac675f6eec3ba7cd74
Martín <magadiflo@gmail.com>
2024-09-24 13:35:19 -0500

    Mono - FromRunnable

--- src/main/java/dev/magadiflo/app/common/Util.java
+++ src/main/java/dev/magadiflo/app/common/Util.java
@@ -1,13 +1,21 @@
 package dev.magadiflo.app.common;
 
+import com.github.javafaker.Faker;
 import org.reactivestreams.Subscriber;
 
 public class Util {
+
+    private static final Faker faker = Faker.instance();
+
     public static <T> Subscriber<T> subscriber() {
         return new DefaultSubscriber<>("");
     }
 
     public static <T> Subscriber<T> subscriber(String name) {
         return new DefaultSubscriber<>(name);
+    }
+
+    public static Faker faker() {
+        return faker;
     }
 }
