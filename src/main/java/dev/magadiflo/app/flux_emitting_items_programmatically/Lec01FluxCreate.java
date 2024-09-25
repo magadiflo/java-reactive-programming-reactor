7b786ba0c3e6ca3c6142cf82466e5aa3b097fda8
Martín <magadiflo@gmail.com>
2024-09-25 17:32:54 -0500

    Flux Create

---
+++ src/main/java/dev/magadiflo/app/flux_emitting_items_programmatically/Lec01FluxCreate.java
@@ -0,0 +1,22 @@
+package dev.magadiflo.app.flux_emitting_items_programmatically;
+
+import dev.magadiflo.app.common.Util;
+import reactor.core.publisher.Flux;
+
+public class Lec01FluxCreate {
+    public static void main(String[] args) {
+        /**
+         * A veces podemos tener un requisito donde nos pidan seguir haciendo algo
+         * hasta que se cumpla cierta condición.
+         */
+        Flux<String> nameFlux = Flux.create(fluxSink -> {
+            String country;
+            do {
+                country = Util.faker().country().name();
+                fluxSink.next(country);
+            } while (!country.equalsIgnoreCase("Peru"));
+            fluxSink.complete();
+        });
+        nameFlux.subscribe(Util.subscriber());
+    }
+}
