93b2d127e684477571e067ba446ee1231e005c70
Martín <magadiflo@gmail.com>
2024-09-25 11:16:44 -0500

    Flux Range

---
+++ src/main/java/dev/magadiflo/app/flux/Lec05FluxRange.java
@@ -0,0 +1,16 @@
+package dev.magadiflo.app.flux;
+
+import dev.magadiflo.app.common.Util;
+import reactor.core.publisher.Flux;
+
+public class Lec05FluxRange {
+    public static void main(String[] args) {
+        Flux<Integer> range = Flux.range(1, 10);
+        range.subscribe(Util.subscriber());
+
+        // Generamos 10 nombres aleatorios usando la librería Faker
+        Flux<String> names = Flux.range(1, 10)
+                .map(value -> Util.faker().name().firstName());
+        names.subscribe(Util.subscriber());
+    }
+}
