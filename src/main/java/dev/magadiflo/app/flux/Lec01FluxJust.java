37f24cf9d01636f50c55a3d6eaf45cccb1856aa0
Martín <magadiflo@gmail.com>
2024-09-25 10:19:14 -0500

    Flux just

---
+++ src/main/java/dev/magadiflo/app/flux/Lec01FluxJust.java
@@ -0,0 +1,11 @@
+package dev.magadiflo.app.flux;
+
+import dev.magadiflo.app.common.Util;
+import reactor.core.publisher.Flux;
+
+public class Lec01FluxJust {
+    public static void main(String[] args) {
+        Flux.just(1, 2, 3, 4)
+                .subscribe(Util.subscriber());
+    }
+}
