913067a430fc038c3bad2a981fc774a6fae1746d
Martín <magadiflo@gmail.com>
2024-09-26 19:01:44 -0500

    Flux Generate

---
+++ src/main/java/dev/magadiflo/app/flux_emitting_items_programmatically/Lec06FluxGenerate.java
@@ -0,0 +1,19 @@
+package dev.magadiflo.app.flux_emitting_items_programmatically;
+
+import dev.magadiflo.app.common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+
+public class Lec06FluxGenerate {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec06FluxGenerate.class);
+
+    public static void main(String[] args) {
+        Flux.generate(synchronousSink -> {
+                    synchronousSink.next(1);
+                    synchronousSink.complete();
+                })
+                .subscribe(Util.subscriber());
+    }
+}
